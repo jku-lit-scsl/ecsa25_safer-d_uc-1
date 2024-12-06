49a8bf4fcd5bfeb657dba86514e52b48f541c6a3
Marco Stadler <marco.stadler@jku.at>
2024-12-06 17:05:13 +0100

    interm commit
    Signed-off-by: Marco Stadler <marco.stadler@jku.at>

--- README.md
+++ README.md
@@ -3,9 +3,5 @@
 required packages
 
 ```
-pip install flask
-pip install requests
-pip install python-statemachine
-pip install psutil
-pip install pytz
+pip install flask requests python-statemachine psutil pytz
 ```
--- main/main.py
+++ main/main.py
@@ -1,4 +1,7 @@
 # Created on 06.12.2024
+import requests
+
+import config.config as CONFIG
 from util.utils import setup_logging
 
 ips_to_check = []
@@ -9,12 +12,22 @@ def check_adaptations():
     pass
 
 
+def traverse_parent_ips(parent_ip):
+    # add ips to the list
+    ips_to_check.append(parent_ip)
+    response = requests.get(f"http://{parent_ip}:5000/get_parent_ip")
+    print("Response from /:", response.text)
+    pass
+
+
 def init_ip_tree():
     """
     Traverses once through the hierarchy and gets the ip addresses to check for adaptation
     :return: void
     """
-    pass
+    if CONFIG.parent_ip != '':
+        traverse_parent_ips(parent_ip=CONFIG.parent_ip)
+
 
 
 if __name__ == "__main__":
