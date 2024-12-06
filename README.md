d1d8f715d03387921e731485a0f388be8def72c0
Marco Stadler <marco.stadler@jku.at>
2024-12-06 17:12:06 +0100

    fixed python version bug
    Signed-off-by: Marco Stadler <marco.stadler@jku.at>

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 required packages
 
+
 ```
 pip install flask requests python-statemachine psutil pytz
 ```
+
+Note: `pip3` on mac
--- config/config.py
+++ config/config.py
@@ -13,23 +13,19 @@ def get_local_ip():
 
 
 def determine_parent_ip(ip):
-    match ip:
-        case "192.168.56.20":
-            return ''
-        case "192.168.56.24":
-            return '192.168.56.20'
-        case "192.168.56.28":
-            return '192.168.56.20'
-        case _:
-            logging.error(f'Could not determine parent IP for {ip}')
+    if ip == "192.168.56.20":
+        return ''
+    elif "192.168.56.24" or "192.168.56.28":
+        return '192.168.56.20'
+    else:
+        logging.error(f'Could not determine parent IP for {ip}')
 
 
 def determine_child_ip(ip):
-    match ip:
-        case "192.168.56.20":
-            return ['192.168.56.24', '192.168.56.28']
-        case _:
-            logging.error(f'Could not determine child IPs for {ip}')
+    if ip == "192.168.56.20":
+        return ['192.168.56.24', '192.168.56.28']
+    else:
+        logging.error(f'Could not determine child IPs for {ip}')
 
 
 my_ip = get_local_ip()
