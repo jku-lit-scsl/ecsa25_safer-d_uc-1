5eea93b60fd628d6ae09e0236b5e17e1046be65c
Marco Stadler <marco.stadler@jku.at>
2024-12-08 18:50:29 +0100

    fixed url bug
    Signed-off-by: Marco Stadler <marco.stadler@jku.at>

--- runner.py
+++ runner.py
@@ -8,7 +8,7 @@ import requests
 
 def send_http_request():
     try:
-        response = requests.get("192.168.56.20:5000/in_sos_mode")
+        response = requests.get("http://192.168.56.20:5000/in_sos_mode")
         if response.status_code == 200 and bool(json.loads(response.text)['is_in_SoS_mode']):
             return True
     except Exception as e:
